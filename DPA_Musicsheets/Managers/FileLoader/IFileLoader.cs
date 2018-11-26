9e82cfb303c81991cfcc6e362ad25e7c796a99e4
DanielRichtersz <digidi-daantje@hotmail.com>
2018-11-26 16:05:04 +0100

    Van laptop naar desktop

--- DPA_Musicsheets/Managers/FileLoader/IFileLoader.cs
+++ DPA_Musicsheets/Managers/FileLoader/IFileLoader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DPA_Musicsheets.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,6 @@ namespace DPA_Musicsheets.Managers
 {
 	interface IFileLoader
 	{
-
-		string fileToString(string path);
+		Track fileToTrack(string path);
 	}
 }
--- DPA_Musicsheets/Managers/FileLoader/LilyPondLoader.cs
+++ DPA_Musicsheets/Managers/FileLoader/LilyPondLoader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DPA_Musicsheets.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace DPA_Musicsheets.Managers.FileLoader
 {
 	class LilyPondLoader : IFileLoader
 	{
-		public string fileToString(string path)
+		public Track fileToTrack(string path)
 		{
 			StringBuilder sb = new StringBuilder();
 			foreach (var line in File.ReadAllLines(path))
@@ -17,7 +18,59 @@ namespace DPA_Musicsheets.Managers.FileLoader
 				sb.AppendLine(line);
 			}
 
-			return sb.ToString();
+            string[] stringParts = stringToParts(sb.ToString());
+
+            CreateTrackFromStringParts(stringParts);
+
+            Track track = new Track(); ;
+			return track;
 		}
+
+        private void CreateTrackFromStringParts(string[] stringParts)
+        {
+            Track track = new Track();
+
+            //Reserved properties
+            int playAmounts = 1;
+            int startRepeat = 0;
+            int endRepeat = 0;
+
+            Tuple<int, int> time;
+            int timeValue1 = 0;
+            int timeValue2 = 0;
+
+
+            for (int i = 0; i < stringParts.Length; ++i)
+            {
+                // Call strategies to convert string to objects
+                // Reserved words
+                if (stringParts[i] == "{")
+                {
+
+                }
+
+                if (stringParts[i] == "/repeat")
+                {
+                    int.TryParse(stringParts[i + 2], out playAmounts);
+                    startRepeat = i + 4;
+                }
+                if (stringParts[i] == "}" && startRepeat != 0 && endRepeat == 0)
+                {
+                    endRepeat = i;
+                    // Encountered end of repeat
+                    // Create a copy of the entire repeat
+                    Staff repeatStaff = 
+                }
+
+
+            }
+        }
+
+        public string[] stringToParts(string lilyPondString)
+        {
+            return lilyPondString.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+        }
 	}
+
+
 }
